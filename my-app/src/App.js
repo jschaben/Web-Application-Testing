92d1d994ecac15828c8fa00ab4892fa6272efe42
Josh schaben <jschaben42@gmail.com>
2020-01-23 17:43:47 -0600

    added in tests

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Display from "./components/display.js";
+import Dashboard from "./components/dashboard.js";
+
 function App() {
+  const [homeHits, setHomeHits] = useState(0);
+  const [awayHits, setAwayHits] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [outs, setOuts] = useState(0);
+  const [whosUp, setWhosUp] = useState("Home");
+  const [innings, setInnings] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Display
+        homeHits={homeHits}
+        awayHits={awayHits}
+        balls={balls}
+        strikes={strikes}
+        outs={outs}
+        whosUp={whosUp}
+        innings={innings}
+      />
+      <Dashboard
+        setHomeHits={setHomeHits}
+        setAwayHits={setAwayHits}
+        setBalls={setBalls}
+        setStrikes={setStrikes}
+        setOuts={setOuts}
+        setWhosUp={setWhosUp}
+        setInnings={setInnings}
+        balls={balls}
+        strikes={strikes}
+        outs={outs}
+        whosUp={whosUp}
+        homeHits={homeHits}
+        awayHits={awayHits}
+      />
     </div>
   );
 }
-
 export default App;
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+// import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Display from "./components/display";
+import Dashboard, { addBall, addStrike } from "./components/dashboard.js";
+
+test('renders display component', () => {
+  const { getByText } = render(<Display />);
+  const displayElement = getByText(/home/i);
+  expect(displayElement).toBeInTheDocument();
+});
+
+test("renders dashboard component", () => {
+  const { getByText } = render(<Dashboard />);
+  const dashboardElement = getByText(/ball/i);
+  expect(dashboardElement).toBeInTheDocument();
+});
+
+describe("balls and strikes reset to 0 when you hit 3 strikes or 4 balls", () => {
+  it("balls resets properly to 0", () => {
+    
+    const expected = 0;
+    const ballcount = 3;
+    const actual = addBall(ballcount);
+
+    expect(actual).toBe(expected);
+  });
+
+  it("strikes resets properly to 0", () => {
+    const expected = 0;
+    const strikecount = 2;
+    const actual = addStrike(strikecount, 0);
+
+    expect(actual[0]).toBe(expected);
+  });
 });
